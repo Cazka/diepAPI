503781a9ac38d39be3373b7a04da5cd5dea7dbab
Cazka <can_oe@me.com>
2021-08-26 23:35:10 +0200

    initial playermovement

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import Vector from './src/vector.mjs';
 import Minimap from './src/minimap.mjs';
 import ArenaScaling from './src/arenaScaling.mjs';
 import Arena from './src/arena.mjs';
+import PlayerMovement from './src/playerMovement.mjs';
 
-export { Vector, Minimap, ArenaScaling, Arena };
+export { Vector, Minimap, ArenaScaling, Arena, PlayerMovement };
