d4db82008defe98ffdb97da34a4521fb5ade1781
Cazka <can_oe@me.com>
2021-09-06 01:08:17 +0200

    use ts

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,18 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './index.js',
+    entry: './index.mjs',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.mjs', '.js'],
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'diepAPI.js',
