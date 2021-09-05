a7448aabcbd2f9ed884078de0071e27bf129dfe3
Cazka <can_oe@me.com>
2021-09-06 01:18:17 +0200

    index moved to src folder

--- index.mjs
+++
@@ -1,9 +0,0 @@
-import Vector from './src/vector.mjs';
-import CanvasKit from './src/canvasKit.mjs';
-import DiepGamepad from './src/diepGamepad.mjs';
-import Minimap from './src/minimap.mjs';
-import ArenaScaling from './src/arenaScaling.mjs';
-import Arena from './src/arena.mjs';
-import PlayerMovement from './src/playerMovement.mjs';
-
-export { Vector, CanvasKit, DiepGamepad, Minimap, ArenaScaling, Arena, PlayerMovement };
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './index.mjs',
+    entry: './src/index.mjs',
     module: {
         rules: [
             {
