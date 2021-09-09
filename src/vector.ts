0e132dd15fb203bff9de41cd5198ac92cffe8abb
Cazka <can_oe@me.com>
2021-09-09 05:31:24 +0200

    now good ts

--- src/vector.ts
+++ src/vector.ts
@@ -1,37 +1,9 @@
 export default class Vector {
-    public x: number;
-    public y: number;
-
-    /**
-     * Vector is immutable
-     *
-     * @param {number} x
-     * @param {number} y
-     */
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
-        Object.freeze(this);
-    }
-
-    /**
-     * Factory function
-     *
-     * @param {number} x
-     * @param {number} y
-     * @returns {Vector}
-     */
-    static from(x: number, y: number): Vector {
-        return new Vector(x, y);
-    }
+    constructor(readonly x: number, readonly y: number) {}
 
     /**
      * ( u1 ) + ( v1 ) = ( u1 + v1 )
      * ( u2 )   ( v2 )   ( u2 + v2 )
-     *
-     * @param {Vector} u
-     * @param {Vector} v
-     * @returns {Vector}
      */
     static add(u: Vector, v: Vector): Vector {
         return new Vector(u.x + v.x, u.y + v.y);
@@ -40,10 +12,6 @@ export default class Vector {
     /**
      * ( u1 ) - ( v1 ) = ( u1 - v1 )
      * ( u2 )   ( v2 )   ( u2 - v2 )
-     *
-     * @param {Vector} u
-     * @param {Vector} v
-     * @returns {Vector}
      */
     static subtract(u: Vector, v: Vector): Vector {
         return new Vector(u.x - v.x, u.y - v.y);
@@ -52,10 +20,6 @@ export default class Vector {
     /**
      * ( u1 ) * ( v1 ) = ( u1 * v1 )
      * ( u2 )   ( v2 )   ( u2 * v2 )
-     *
-     * @param {Vector} u
-     * @param {Vector} v
-     * @returns {Vector}
      */
     static multiply(u: Vector, v: Vector): Vector {
         return new Vector(u.x * v.x, u.y * v.y);
@@ -64,10 +28,6 @@ export default class Vector {
     /**
      * ( u1 ) / ( v1 ) = ( u1 / v1 )
      * ( u2 )   ( v2 )   ( u2 / v2 )
-     *
-     * @param {Vector} u
-     * @param {Vector} v
-     * @returns {Vector}
      */
     static divide(u: Vector, v: Vector): Vector {
         return new Vector(u.x / v.x, u.y / v.y);
@@ -76,47 +36,25 @@ export default class Vector {
     /**
      * r * ( v1 ) = ( r * v1 )
      *     ( v2 )   ( r * v2 )
-     *
-     * @param {number} r
-     * @param {Vector} v
-     * @returns {Vector}
      */
     static scale(r: number, v: Vector): Vector {
         return new Vector(r * v.x, r * v.y);
     }
 
-    /**
-     *
-     * @param {Vector} v
-     * @returns {Vector}
-     */
     static round(v: Vector): Vector {
         return new Vector(Math.round(v.x), Math.round(v.y));
     }
-    /**
-     *
-     * @param {Vector} v
-     * @returns {number}
-     */
+
     static len(v: Vector): number {
         return Math.sqrt(v.x ** 2 + v.y ** 2);
     }
 
-    /**
-     *
-     * @param {Vector} u
-     * @param {Vector} v
-     * @returns {number} The Euclidean distance between the two vectors
-     */
     static distance(u: Vector, v: Vector): number {
         return Vector.len(Vector.subtract(u, v));
     }
 
     /**
      * Calculates the [centroid](https://en.wikipedia.org/wiki/Centroid)
-     *
-     * @param  {...Vector} vertices
-     * @returns {Vector}
      */
     static centroid(...vertices: Vector[]): Vector {
         const sum = vertices.reduce((acc, vec) => Vector.add(acc, vec), new Vector(0, 0));
@@ -127,9 +65,7 @@ export default class Vector {
     }
 
     /**
-     *
-     * @param  {...Vector} vertices
-     * @returns {number}
+     * Calcutes the radius from a set of vertices that are placed on a circle
      */
     static radius(...vertices: Vector[]): number {
         const centroid = Vector.centroid(...vertices);
