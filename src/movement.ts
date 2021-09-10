126ef5e571250120c0286a916028f57f823fcbf1
Cazka <can_oe@me.com>
2021-09-10 03:41:00 +0200

    to ts

---
+++ src/movement.ts
@@ -0,0 +1,45 @@
+import { Vector } from './vector';
+
+export class Movement {
+    #position = new Vector(0, 0);
+    #velocity = new Vector(0, 0);
+
+    /*
+     * used for average velocity calculation
+     */
+    readonly #velocitySamplesSize = 10;
+    #velocitySamples = Array(this.#velocitySamplesSize).fill(new Vector(0, 0));
+    #velocitySamplesIndex = 0;
+    #velocityLastNow = performance.now();
+
+    get position(): Vector {
+        return this.#position;
+    }
+
+    get velocity(): Vector {
+        return this.#velocity;
+    }
+
+    updatePos(newPos: Vector): void {
+        this.#updateVelocity(newPos);
+        this.#position = newPos;
+    }
+
+    #updateVelocity(newPos: Vector): void {
+        const now = performance.now();
+        const time = (now - this.#velocityLastNow) / 1000;
+        this.#velocityLastNow = now;
+
+        const velocity = Vector.scale(1 / time, Vector.subtract(newPos, this.#position));
+
+        // add current velocity to our samples array
+        this.#velocitySamples[this.#velocitySamplesIndex++] = velocity;
+        this.#velocitySamplesIndex %= this.#velocitySamples.length;
+
+        // calculate the average velocity
+        this.#velocity = Vector.scale(
+            1 / this.#velocitySamples.length,
+            this.#velocitySamples.reduce((acc, x) => Vector.add(acc, x))
+        );
+    }
+}
