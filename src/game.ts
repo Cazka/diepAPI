4a7d1463aee32a8772f1bc32c42baf1b3f2f6bb2
Cazka <can_oe@me.com>
2021-09-10 04:14:09 +0200

    initial game.ts

---
+++ src/game.ts
@@ -0,0 +1,27 @@
+import { EventEmitter } from './event_emitter';
+import { CanvasKit } from './canvas_kit';
+
+class Game extends EventEmitter {
+    #ready = false;
+
+    constructor() {
+        super();
+
+        CanvasKit.hookRAF(() => this.#onframe());
+    }
+
+    #onframe(): void {
+        if (!this.#ready && (window as any).input !== undefined) {
+            this.#ready = true;
+            this.#onready();
+        }
+
+        super.emit('frame');
+    }
+
+    #onready(): void {
+        super.emit('ready');
+    }
+}
+
+export const game = new Game();
