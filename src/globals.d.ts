70f358f64b9ef52a64a8f160fb70c105fdee1c9c
Cazka <can_oe@me.com>
2021-09-10 04:28:50 +0200

    add types

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,1 +1,16 @@
-declare var input: object;
+interface Input {
+    mouse: (x: number, y: number) => void;
+    keyDown: (key: string | number) => void;
+    keyUp: (key: string | number) => void;
+    blur: Function; //_reset_keys
+    wheel: Function; //_mouse_wheel
+    prevent_right_click: (value: boolean) => void; //_prevent_right_click
+    flushInputHooks: Function; //_flush_input_hooks
+    set_convar: (key: string, value: string) => boolean;
+    get_convar: (key: string) => null | string;
+    execute: (v: string) => void;
+    print_convar_help: () => void;
+    should_prevent_unload: () => boolean;
+}
+
+declare var input: Input;
