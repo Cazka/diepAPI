cd37a2a7b078102a6d7c77aa3310cd4c51ef91d7
Cazka <can_oe@me.com>
2021-09-14 03:46:29 +0200

    add more stuff

--- src/entity.ts
+++ src/entity.ts
@@ -19,8 +19,14 @@ export enum EntityType {
  * Holds minimal information currently.
  */
 export class Entity extends Movement {
+    type: EntityType;
     constructor(type: EntityType, position: Vector) {
         super();
         super.updatePos(position);
+        this.type = type;
+    }
+
+    updatePos(newPos: Vector): void {
+        super.updatePos(newPos);
     }
 }
