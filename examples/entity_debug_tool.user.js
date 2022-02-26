5ea1bb2f12e4e496537dd94c04511b08f77e9b97
Cazka <can_oe@me.com>
2022-02-26 22:58:46 +0100

    updated tool

--- examples/entity_debug_tool.user.js
+++ examples/entity_debug_tool.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Entity Debug Tool
 // @description  https://github.com/Cazka/diepAPI
-// @version      1.0.0
+// @version      1.0.1
 // @author       Cazka
 // @match        https://diep.io/*
 // @icon         https://www.google.com/s2/favicons?domain=diep.io
@@ -10,7 +10,7 @@
 // ==/UserScript==
 if (!window.diepAPI) return window.alert('Please install diepAPI to use this script');
 
-const { entityManager, game, arenaScaling, Vector, CanvasKit, player } = window.diepAPI;
+const { entityManager, game, arenaScaling, Vector, CanvasKit } = window.diepAPI;
 
 class EntityOverlay {
     #canvas;
@@ -24,15 +24,15 @@ class EntityOverlay {
         this.#onResize();
     }
     #onResize() {
-        this.#canvas.width = window.innerWidth;
-        this.#canvas.height = window.innerHeight;
+        this.#canvas.width = window.innerWidth * window.devicePixelRatio;
+        this.#canvas.height = window.innerHeight * window.devicePixelRatio;
     }
     #onFrame() {
-        this.#ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
         entityManager.entities.forEach((entity) => {
-            const position = arenaScaling.toScreenPos(entity.position);
-            const futurePos = arenaScaling.toScreenPos(entity.predictPos(1000));
-            const dimensions = arenaScaling.toScreenUnits(
+            const position = arenaScaling.toCanvasPos(entity.position);
+            const futurePos = arenaScaling.toCanvasPos(entity.predictPos(1000));
+            const dimensions = arenaScaling.toCanvasUnits(
                 new Vector(2 * entity.extras.radius, 2 * entity.extras.radius)
             );
             this.#ctx.strokeStyle = 9 === entity.type ? '#ffffff' : entity.extras.color;
@@ -51,7 +51,7 @@ class EntityOverlay {
             this.#ctx.lineTo(futurePos.x, futurePos.y);
             this.#ctx.stroke();
             //Time alive + id
-            const fontSize = arenaScaling.toScreenUnits(new Vector(30, 30));
+            const fontSize = arenaScaling.toCanvasUnits(new Vector(30, 30));
             this.#ctx.font = fontSize.x + 'px Ubuntu';
             this.#ctx.fillText(
                 `${entity.extras.id} ${Math.floor((performance.now() - entity.extras.timestamp) / 1000)}`,
