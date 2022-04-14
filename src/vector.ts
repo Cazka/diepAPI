1f72eff45e11964d29709a71fea342d013908a19
Cazka <can_oe@me.com>
2022-04-14 11:41:57 +0200

    add unscale method

--- src/vector.ts
+++ src/vector.ts
@@ -1,52 +1,36 @@
 export class Vector {
     constructor(readonly x: number, readonly y: number) {}
 
-    /**
-     * ( u1 ) + ( v1 ) = ( u1 + v1 )
-     * ( u2 )   ( v2 )   ( u2 + v2 )
-     */
-    static add(u: Vector, v: Vector): Vector {
-        return new Vector(u.x + v.x, u.y + v.y);
-    }
-
-    /**
-     * ( u1 ) - ( v1 ) = ( u1 - v1 )
-     * ( u2 )   ( v2 )   ( u2 - v2 )
-     */
-    static subtract(u: Vector, v: Vector): Vector {
-        return new Vector(u.x - v.x, u.y - v.y);
-    }
-
-    /**
-     * ( u1 ) * ( v1 ) = ( u1 * v1 )
-     * ( u2 )   ( v2 )   ( u2 * v2 )
-     */
-    static multiply(u: Vector, v: Vector): Vector {
-        return new Vector(u.x * v.x, u.y * v.y);
-    }
-
-    /**
-     * ( u1 ) / ( v1 ) = ( u1 / v1 )
-     * ( u2 )   ( v2 )   ( u2 / v2 )
-     */
-    static divide(u: Vector, v: Vector): Vector {
-        return new Vector(u.x / v.x, u.y / v.y);
-    }
-
-    /**
-     * r * ( v1 ) = ( r * v1 )
-     *     ( v2 )   ( r * v2 )
-     */
-    static scale(r: number, v: Vector): Vector {
-        return new Vector(r * v.x, r * v.y);
+    static len(v: Vector): number {
+        return Math.sqrt(v.x ** 2 + v.y ** 2);
     }
 
     static round(v: Vector): Vector {
         return new Vector(Math.round(v.x), Math.round(v.y));
     }
 
-    static len(v: Vector): number {
-        return Math.sqrt(v.x ** 2 + v.y ** 2);
+    static scale(r: number, v: Vector): Vector {
+        return new Vector(r * v.x, r * v.y);
+    }
+
+    static unscale(r: number, v: Vector): Vector {
+        return new Vector(v.x / r, v.y / r);
+    }
+
+    static add(u: Vector, v: Vector): Vector {
+        return new Vector(u.x + v.x, u.y + v.y);
+    }
+
+    static subtract(u: Vector, v: Vector): Vector {
+        return new Vector(u.x - v.x, u.y - v.y);
+    }
+
+    static multiply(u: Vector, v: Vector): Vector {
+        return new Vector(u.x * v.x, u.y * v.y);
+    }
+
+    static divide(u: Vector, v: Vector): Vector {
+        return new Vector(u.x / v.x, u.y / v.y);
     }
 
     static distance(u: Vector, v: Vector): number {
