d53f14c32c6e9845090168fa45d3da088a206631
Cazka <can_oe@me.com>
2022-04-16 16:27:43 +0200

    export entity, color, type

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,5 @@
 export { CanvasKit } from './canvas_kit';
 export { EventEmitter } from './event_emitter';
+export { Entity, EntityColor, EntityType } from './entity';
 export { Movement } from './movement';
 export { Vector } from './vector';
