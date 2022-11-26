a02472d982d3ce126ce10646a6d2ae2681610b25
Cazka <can_oe@me.com>
2022-11-26 22:14:29 +0100

    Feature/use custom input (#57)
    * initial input.ts
    
    * add input to apis
    
    * declare types explicit
    
    * use the new input class

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,6 +1,7 @@
 export { arena } from './arena';
 export { camera } from './camera';
 export { game } from './game';
+export { input } from './input';
 export { minimap } from './minimap';
 export { player } from './player';
 export { playerMovement } from './player_movement';
---
+++ src/apis/input.ts
@@ -0,0 +1,78 @@
+import { game } from '../apis/game';
+
+const sleep = (ms: number): Promise<void> => new Promise((resolve, reject) => setTimeout(resolve, ms));
+
+class Input {
+    #gameCanvas: HTMLCanvasElement;
+
+    constructor() {
+        game.once('ready', () => {
+            this.#gameCanvas = document.getElementById('canvas') as HTMLCanvasElement;
+        });
+    }
+
+    keyDown(key: string | number): void {
+        if (typeof key == 'string') {
+            key = this.#toKeyCode(key);
+        }
+
+        const keydown = new KeyboardEvent('keydown', {
+            key: '',
+            code: '',
+            keyCode: key,
+            which: key,
+            cancelable: true,
+            composed: true,
+            bubbles: true,
+        });
+
+        _window.dispatchEvent(keydown);
+    }
+
+    keyUp(key: string | number): void {
+        if (typeof key == 'string') {
+            key = this.#toKeyCode(key);
+        }
+
+        const keyup = new KeyboardEvent('keyup', {
+            key: '',
+            code: '',
+            keyCode: key,
+            which: key,
+            cancelable: true,
+            composed: true,
+            bubbles: true,
+        });
+
+        _window.dispatchEvent(keyup);
+    }
+
+    async keyPress(key: number): Promise<void> {
+        this.keyDown(key);
+        await sleep(200);
+        this.keyUp(key);
+        await sleep(10);
+    }
+
+    mouse(x: number, y: number): void {
+        const mousemove = new MouseEvent('mousemove', {
+            clientX: x,
+            clientY: y,
+            cancelable: true,
+            composed: true,
+            bubbles: true,
+        });
+
+        this.#gameCanvas.dispatchEvent(mousemove);
+    }
+
+    #toKeyCode(key: string): number {
+        if (key.length != 1) {
+            throw new Error(`diepAPI: Unsupported key: ${key}`);
+        }
+
+        return key.toUpperCase().charCodeAt(0);
+    }
+}
+
+export const input = new Input();
