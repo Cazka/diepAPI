191aabf91d1e6a0906af1fb2041716e1d21e048c
Cazka <can_oe@me.com>
2025-12-04 17:25:42 +0100

    fix: add support for offscreen canvas

--- src/core/canvas_kit.ts
+++ src/core/canvas_kit.ts
@@ -5,13 +5,17 @@ type FunctionKeys<T> = {
   [K in keyof T]: T[K] extends Function ? K : never;
 }[keyof T];
 
+type RenderingContext = CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D;
+
+const CANVAS_KIT_BYPASS = Symbol('CanvasKit-bypass');
+
 export class CanvasKit {
   /**
    * If you need a canvas then create it with this method.
    */
   static createCanvas(): HTMLCanvasElement {
     const canvas = document.createElement('canvas');
-    canvas.className = 'CanvasKit-bypass';
+    (canvas as any)[CANVAS_KIT_BYPASS] = true;
     canvas.style.pointerEvents = 'none';
     canvas.style.position = 'fixed';
     canvas.style.zIndex = '1';
@@ -39,22 +43,22 @@ export class CanvasKit {
   /**
    * The consumer will be called before
    */
-  static hookCtx<T extends FunctionKeys<CanvasRenderingContext2D>>(
+  static hookCtx<T extends FunctionKeys<RenderingContext>>(
     method: T,
     consumer: (
-      target: CanvasRenderingContext2D[T],
-      thisArg: CanvasRenderingContext2D,
-      args: Parameters<CanvasRenderingContext2D[T]>,
+      target: RenderingContext[T],
+      thisArg: RenderingContext,
+      args: Parameters<RenderingContext[T]>,
     ) => void,
+    useOffscreenCtx = false,
   ): void {
-    const target = _window.CanvasRenderingContext2D.prototype;
+    const target = useOffscreenCtx
+      ? _window.OffscreenCanvasRenderingContext2D.prototype
+      : _window.CanvasRenderingContext2D.prototype;
+
     target[method] = new Proxy(target[method], {
-      apply(
-        target,
-        thisArg: CanvasRenderingContext2D,
-        args: Parameters<CanvasRenderingContext2D[T]>,
-      ) {
-        if (thisArg.canvas.className !== 'CanvasKit-bypass') consumer(target, thisArg, args);
+      apply(target, thisArg: RenderingContext, args: Parameters<RenderingContext[T]>) {
+        if (!(thisArg.canvas as any)[CANVAS_KIT_BYPASS]) consumer(target, thisArg, args);
         return Reflect.apply(target, thisArg, args);
       },
     });
@@ -64,22 +68,22 @@ export class CanvasKit {
    * replaces the function. Use `return Reflect.apply(target, thisArg, args);` in
    * your function to call the original function.
    */
-  static overrideCtx<T extends FunctionKeys<CanvasRenderingContext2D>>(
+  static overrideCtx<T extends FunctionKeys<RenderingContext>>(
     method: T,
     func: (
-      target: CanvasRenderingContext2D[T],
-      thisArg: CanvasRenderingContext2D,
-      args: Parameters<CanvasRenderingContext2D[T]>,
+      target: RenderingContext[T],
+      thisArg: RenderingContext,
+      args: Parameters<RenderingContext[T]>,
     ) => unknown,
+    useOffscreenCtx = false,
   ): void {
-    const target = _window.CanvasRenderingContext2D.prototype;
+    const target = useOffscreenCtx
+      ? _window.OffscreenCanvasRenderingContext2D.prototype
+      : _window.CanvasRenderingContext2D.prototype;
+
     target[method] = new Proxy(target[method], {
-      apply(
-        target,
-        thisArg: CanvasRenderingContext2D,
-        args: Parameters<CanvasRenderingContext2D[T]>,
-      ) {
-        if (thisArg.canvas.className !== 'CanvasKit-bypass') return func(target, thisArg, args);
+      apply(target, thisArg: RenderingContext, args: Parameters<RenderingContext[T]>) {
+        if (!(thisArg.canvas as any)[CANVAS_KIT_BYPASS]) return func(target, thisArg, args);
         return Reflect.apply(target, thisArg, args);
       },
     });
@@ -91,43 +95,58 @@ export class CanvasKit {
    */
   static hookPolygon(
     numVertices: number,
-    cb: (vertices: Vector[], ctx: CanvasRenderingContext2D) => void,
+    cb: (vertices: Vector[], ctx: RenderingContext) => void,
+    useOffscreenCtx = false,
   ): void {
     let index = 0;
 
     let vertices: Vector[] = [];
 
-    const onFillPolygon = (ctx: CanvasRenderingContext2D) => {
-      cb(vertices, ctx);
-    };
+    const onFillPolygon = (ctx: RenderingContext) => cb(vertices, ctx);
 
-    CanvasKit.hookCtx('beginPath', (target, thisArg, args) => {
-      index = 1;
-      vertices = [];
-    });
-    CanvasKit.hookCtx('moveTo', (target, thisArg, args) => {
-      if (index === 1) {
-        index++;
-        vertices.push(new Vector(args[0], args[1]));
-        return;
-      }
-      index = 0;
-    });
-    CanvasKit.hookCtx('lineTo', (target, thisArg, args) => {
-      if (index >= 2 && index <= numVertices) {
-        index++;
-        vertices.push(new Vector(args[0], args[1]));
-        return;
-      }
-      index = 0;
-    });
-    CanvasKit.hookCtx('fill', (target, thisArg, args) => {
-      if (index === numVertices + 1) {
-        index++;
-        onFillPolygon(thisArg);
-        return;
-      }
-      index = 0;
-    });
+    CanvasKit.hookCtx(
+      'beginPath',
+      (target, thisArg, args) => {
+        index = 1;
+        vertices = [];
+      },
+      useOffscreenCtx,
+    );
+    CanvasKit.hookCtx(
+      'moveTo',
+      (target, thisArg, args) => {
+        if (index === 1) {
+          index++;
+          vertices.push(new Vector(args[0], args[1]));
+          return;
+        }
+        index = 0;
+      },
+      useOffscreenCtx,
+    );
+    CanvasKit.hookCtx(
+      'lineTo',
+      (target, thisArg, args) => {
+        if (index >= 2 && index <= numVertices) {
+          index++;
+          vertices.push(new Vector(args[0], args[1]));
+          return;
+        }
+        index = 0;
+      },
+      useOffscreenCtx,
+    );
+    CanvasKit.hookCtx(
+      'fill',
+      (target, thisArg, args) => {
+        if (index === numVertices + 1) {
+          index++;
+          onFillPolygon(thisArg);
+          return;
+        }
+        index = 0;
+      },
+      useOffscreenCtx,
+    );
   }
 }
