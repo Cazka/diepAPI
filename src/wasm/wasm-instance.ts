e36af62410c64dc4a138a932737a298453c79846
Cazka <can_oe@me.com>
2025-12-31 17:29:42 +0100

    fix: use correct imports & exports

--- src/wasm/wasm-instance.ts
+++ src/wasm/wasm-instance.ts
@@ -9,40 +9,54 @@ export type WasmInstanceEvents = 'instantiated';
 class WasmInstance extends EventEmitter<WasmInstanceEvents> {
   #instance: WebAssembly.Instance | null = null;
   #module: WebAssembly.Module | null = null;
-  #imports: WebAssembly.ModuleImportDescriptor[] | null = null;
-  #exports: WebAssembly.ModuleExportDescriptor[] | null = null;
+  #importDescriptors: WebAssembly.ModuleImportDescriptor[] | null = null;
+  #exportDescriptors: WebAssembly.ModuleExportDescriptor[] | null = null;
+  #imports: WebAssembly.Imports | null = null;
+  #exports: WebAssembly.Exports | null = null;
 
-  get instance() {
+  get instance(): WebAssembly.Instance | null {
     return this.#instance;
   }
 
-  get module() {
+  get module(): WebAssembly.Module | null {
     return this.#module;
   }
 
-  get imports() {
+  get importDescriptors(): WebAssembly.ModuleImportDescriptor[] | null {
+    return this.#importDescriptors;
+  }
+  get exportDescriptors(): WebAssembly.ModuleExportDescriptor[] | null {
+    return this.#exportDescriptors;
+  }
+
+  get imports(): WebAssembly.Imports | null {
     return this.#imports;
   }
 
-  get exports() {
+  get exports(): WebAssembly.Exports | null {
     return this.#exports;
   }
 
   constructor() {
     super();
     WebAssembly.instantiateStreaming = new Proxy(WebAssembly.instantiateStreaming, {
-      apply: async (target, thisArg, args) => {
-        const result = (await Reflect.apply(
-          target,
-          thisArg,
-          args,
-        )) as WebAssembly.WebAssemblyInstantiatedSource;
+      apply: async (
+        target,
+        thisArg,
+        args: [Response | PromiseLike<Response>, WebAssembly.Imports | undefined],
+      ) => {
+        const [source, importObject] = args;
+
+        const result = await Reflect.apply(target, thisArg, args);
 
         this.#instance = result.instance;
         this.#module = result.module;
 
-        this.#imports = WebAssembly.Module.imports(this.#module);
-        this.#exports = WebAssembly.Module.exports(this.#module);
+        this.#importDescriptors = WebAssembly.Module.imports(this.#module);
+        this.#exportDescriptors = WebAssembly.Module.exports(this.#module);
+
+        this.#imports = importObject ?? null;
+        this.#exports = result.instance.exports;
 
         this.emit('instantiated');
 
